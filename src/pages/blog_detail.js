b6e83e3388c81f33a6532375df4645e9ef5818a2
SaiZayarHtet7 <saizayarhtet7@gmail.com>
2023-09-30 23:31:36 +0630

    404 not found

--- src/pages/blog_detail.js
+++ src/pages/blog_detail.js
@@ -1,12 +1,24 @@
-import { useParams } from "react-router-dom";
-import './home.css';
+import { useNavigate, useParams } from "react-router-dom";
+import "./home.css";
 import useFetch from "../hooks/useFetch";
+import { useEffect } from "react";
 
 function BlogDetail() {
   let id = useParams().id;
 
   let url = "http://localhost:3001/blogs/" + id;
-  let { data:blog, loading, error } = useFetch(url);
+  let { data: blog, loading, error } = useFetch(url);
+
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    if (error) {
+      ///redirecting to homepage due to error
+      setTimeout(() => {
+        navigate("/");
+      }, 2000);
+    }
+  }, [error, navigate]);
 
   return (
     <div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,38 +1,40 @@
-
-import {
-    createBrowserRouter,
-  } from "react-router-dom";
-  import App from "../App";
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
 import Home from "../pages/home";
 import About from "../pages/about";
 import Contact from "../pages/contact";
 import BlogDetail from "../pages/blog_detail";
+import NotFound from "../pages/not_found";
 
-  const router = createBrowserRouter([
-    {
-      path: "/", // localhost :3000
-      element: <App/>, 
-      children:[
-        {
-            path:"",// localhost :3000,
-            element:<Home/>
-            // element: ,
-        },
-        {
-            path:"/about",// localhost :3000/about,
-            element:<About/>
-        },
-        {
-          path:"/contact",// localhost :3000/about,
-          element:<Contact/>
-      },{
-        path:"/blogs/:id",// localhost :3000/about,
-        element:<BlogDetail/>
-    },
-        
-      ]
-    },
-  ]);
-  
+const router = createBrowserRouter([
+  {
+    path: "/", // localhost :3000
+    element: <App />,
+    children: [
+      {
+        path: "", // localhost :3000,
+        element: <Home />,
+        // element: ,
+      },
+      {
+        path: "/about", // localhost :3000/about,
+        element: <About />,
+      },
+      {
+        path: "/contact", // localhost :3000/about,
+        element: <Contact />,
+      },
+      {
+        path: "/blogs/:id", // localhost :3000/about,
+        element: <BlogDetail />,
+      },
+      {
+        path: "*", // fallback when unknown route
+        element: <NotFound/>
 
-  export default router;
+      }
+    ],
+  },
+]);
+
+export default router;
